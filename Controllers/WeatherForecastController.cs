46c8e9d94f2d8078ae0958f898acc4d4e4e5a775
hazi.bence <hazi.bence@sdadms.hu>
2023-09-20 15:14:38 +0200

    Update terraform main file

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "xxx", "yyy", "zzz", "xxx", "yyy", "zzz", "xxx", "yyy", "zzz", "xxx"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
