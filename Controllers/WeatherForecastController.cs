e061538ea96610573c125c7301d4871da1777463
hazi.bence <hazi.bence@sdadms.hu>
2023-09-20 12:42:44 +0200

    Status comment

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -6,6 +6,7 @@ namespace weatherapi.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
+    // Status strings
     private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
