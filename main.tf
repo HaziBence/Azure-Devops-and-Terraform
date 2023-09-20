6674d469f634b8eaa76ae83eda5490360a584642
hazi.bence <hazi.bence@sdadms.hu>
2023-09-20 14:49:07 +0200

    Updated pipeline file

--- main.tf
+++ main.tf
@@ -12,6 +12,16 @@ provider "azurerm" {
   features {}
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tf_resourceGroup_blobStore"
+    storage_account_name = "hazibterraformstorage"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
+
 resource "azurerm_resource_group" "tf_test" {
   name     = "tfmainrg"
   location = "West Europe"
